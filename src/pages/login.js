a1ac894dc8c3da5419a43d9aa8b47d03f7694830
Ali M <ali.marinas@icloud.com>
2021-10-08 23:42:12 -0700

    add route when user is accepted go to dashboard

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 
@@ -9,6 +9,7 @@ import Logo from "../images/logo.png";
 
 
 export default function Login() {
+    const history= useHistory();
     const { firebase } = useContext(FirebaseContext);
     
     const [emailAddress, setEmailAddress] = useState('');
@@ -22,6 +23,7 @@ export default function Login() {
         
         try {
             await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+            history.push(ROUTES.DASHBOARD);
         } catch (error) {
             setEmailAddress('');
             setPassword('');
