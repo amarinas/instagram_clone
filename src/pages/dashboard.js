61a9c3fed815d6300090348502f29080e4921c73
Ali M <ali.marinas@icloud.com>
2021-10-28 14:49:36 -0700

    started on dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Header from '..//components/header.js';
+import Timeline from '..//components/timeline.js';
+import Sidebar from  '..//components/sidebar/index.js';
+
 
 export default function Dashboard() {
-    return (
-        <p>Hello from Dashboard</p>
+    useEffect(() => {
+        document.title ='Instagram';
+    }, []);
 
+    return (
+        <div className="bg-gray-100">
+            <Header />
+            <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+                <Timeline />
+                <Sidebar />
+            </div>
+        </div>
     )
+    
 }
