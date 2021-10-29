964bc84571941d8d6b39b1657119cd3827cafbf6
Ali M <ali.marinas@icloud.com>
2021-10-29 13:00:08 -0700

    login and sign up button on dashboard

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,10 @@ import * as ROUTES from '../constants/routes';
 
 
 export default function Header() {
-
+    const { firebase } = useContext(FirebaseContext);
+    const user = {
+        displayName: "karl"
+    };
     
     return (
     
@@ -19,6 +22,53 @@ export default function Header() {
                                 <img src={Logo} alt="instagram logo" className="mt-2 w-6/12" />
                             </Link>
                         </h1>
+                    </div>
+                    <div className="text-gray text-center flex items-center align-items">
+                        {user ? (
+                            <>
+                                <Link to={ROUTES.DASHBOARD} aria-label="Home">
+                                    <p>Dashboard</p>
+                                </Link>
+                                <button 
+                                    type="button"
+                                    className="Sign Out"
+                                    onClick={() => firebase.auth().signOut()}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter'){
+                                            firebase.auth().signOut();
+                                        }
+                                    }}
+                                >
+                                    Sign Out
+                                </button>
+                                <div className="flex items-center cursor-pointer">
+                                    <Link to={`/p/${user.displayName}`}>
+                                        <img
+                                            className="rounded-full h-8 w-8 flex"
+                                            src={`/images/avatars/${user.displayName}.jpg`}
+                                            alt={`${user.displayName}`}
+                                        />
+                                    </Link>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <Link to={ROUTES.LOGIN}>
+                                    <button 
+                                        type="button"
+                                        className="bg-blue font-bold text-sm rounded text-white w-20 h-8">
+                                        Log In
+                                    </button>
+                                </Link>
+                                <Link to={ROUTES.SIGN_UP}>
+                                    <button 
+                                        type="button"
+                                        className="font-bold text-sm rounded text-blue w-20 h-8">
+                                            Sign Up
+                                    </button>
+                                </Link>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
